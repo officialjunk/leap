c9d81dc51e2e3b00fea412bf612b5c20d7538fa5
officialjunk <officialjunk@gmail.com>
2011-04-02 15:48:01 -0700

    t=0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leap::Application.config.secret_token = '40f75eef8697097901e91c729c366029b41f62e30df998318158f71e0a1f594039c037865e6bd7261e82da74facdddd1960141cec5dd9adffb15ac5fad4e5c0f'
